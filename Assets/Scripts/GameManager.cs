1e3aaa083f6e0d09f2563055b53632c49693023d
stranker <dani_mrx93@hotmail.com>
2018-05-18 22:44:02 -0300

    Se agregan Standar Asset y principio generacion nivel

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private int[,] matrizMapa;
+    private int largoMapa;
+    private int anchoMapa;
+    // Use this for initialization
+    void Start () {
+        largoMapa = 5;
+        anchoMapa = 5;
+        matrizMapa = new int[largoMapa, anchoMapa];
+        GenerarMapa();
+        MostrarMapa();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GenerarMapa()
+    {
+        for (int i = 0; i < largoMapa; i++)
+        {
+            for (int j = 0; j < anchoMapa; j++)
+            {
+                if (i == 0 || i == anchoMapa - 1 || j == 0 || j == largoMapa - 1)
+                {
+                    matrizMapa[i, j] = 1;
+                }
+            }
+        }
+    }
+
+    public void MostrarMapa()
+    {
+        for (int i = 0; i < largoMapa; i++)
+        {
+            Debug.Log(matrizMapa[i, 0] + " " + matrizMapa[i, 1] + " " + matrizMapa[i, 2] + " " + matrizMapa[i, 3] + " " + matrizMapa[i, 4]);
+        }
+    }
+}
