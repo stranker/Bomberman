d1f6080c7b20b681390155922a9014c138c42a31
stranker <dani_mrx93@hotmail.com>
2018-05-19 19:03:23 -0300

    Se hace el generador de nivel

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-
+    public GameObject cajaIndestructible;
+    public GameObject piso;
     private int[,] matrizMapa;
     private int largoMapa;
     private int anchoMapa;
     // Use this for initialization
     void Start () {
-        largoMapa = 5;
-        anchoMapa = 5;
+        largoMapa = 15;
+        anchoMapa = 13;
         matrizMapa = new int[largoMapa, anchoMapa];
         GenerarMapa();
-        MostrarMapa();
 	}
 	
 	// Update is called once per frame
@@ -27,19 +27,22 @@ public class GameManager : MonoBehaviour {
         {
             for (int j = 0; j < anchoMapa; j++)
             {
-                if (i == 0 || i == anchoMapa - 1 || j == 0 || j == largoMapa - 1)
-                {
+                if (i == 0 || i == largoMapa - 1 || j == 0 || j == anchoMapa - 1 || (i % 2 == 0 && j % 2 == 0))
                     matrizMapa[i, j] = 1;
+            }
+        }
+        for (int i = 0; i < largoMapa; i++)
+        {
+            for (int j = 0; j < anchoMapa; j++)
+            {
+                if (matrizMapa[i,j] == 1)
+                {
+                    Instantiate(cajaIndestructible, new Vector3(i + 0.5f, cajaIndestructible.transform.position.y, j + 0.5f),transform.rotation,transform.parent);
                 }
             }
         }
-    }
-
-    public void MostrarMapa()
-    {
-        for (int i = 0; i < largoMapa; i++)
-        {
-            Debug.Log(matrizMapa[i, 0] + " " + matrizMapa[i, 1] + " " + matrizMapa[i, 2] + " " + matrizMapa[i, 3] + " " + matrizMapa[i, 4]);
-        }
+        GameObject clon = Instantiate(piso);
+        clon.transform.position = new Vector3(largoMapa,0,anchoMapa)/2;
+        clon.transform.localScale = new Vector3(largoMapa, 1, anchoMapa)/10;
     }
 }
