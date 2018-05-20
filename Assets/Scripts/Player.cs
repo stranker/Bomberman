48bd3d7f12d460d368916177c02fb4e1996dfa6a
stranker <dani_mrx93@hotmail.com>
2018-05-19 23:03:55 -0300

    Se agrega player y se codifica su movimiento

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    private Vector3 movimiento;
+    private int speed = 100;
+	// Use this for initialization
+	void Start () {
+        movimiento = Vector3.zero;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        movimiento.x = Input.GetAxis("Horizontal") * Time.deltaTime * speed; ;
+        movimiento.z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+        GetComponent<Rigidbody>().velocity = movimiento;
+    }
+}
